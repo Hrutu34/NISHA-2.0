9880810da3d9e5bc9785f2a5257ddc4ff2a19e8c
Hrutu Hemant Surve <survehrutu@gmail.com>
2026-08-19 01:15:20 +0530

    App was able to build but fixing issues with langchain community libs

--- src/rag_engine.py
+++ src/rag_engine.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_chroma import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
